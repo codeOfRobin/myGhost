6997eff553ca120201cc47867736cd549a53f0ed
Robin Malhotra <malhotra.robin@icloud.com>
2016-04-06 18:05:23 +0530

    sdafsd

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://comicsans.me',
+        url: 'http://localhost:2368',
         mail: {},
         database: {
             client: 'sqlite3',
