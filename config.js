e111bddd90e53da5afd77a133902e0cf282e0df3
Robin Malhotra <malhotra.robin@icloud.com>
2016-04-06 20:49:12 +0530

    fixed config

--- config.js
+++ config.js
@@ -11,12 +11,12 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://localhost:2368',
+        url: 'http://comicsans.me',
         mail: {},
         database: {
             client: 'sqlite3',
             connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
             },
             debug: false
         },
